3a3e698f4fb336ecdcdd99847162132677699379
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-03-06 23:13:26 +0100

    Reorganise app code
    Enclose sections in own components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import { useLang, useMeta, useTitle } from 'hoofd/preact';
 import { Provider } from 'redux-zero/preact';
 
-import { ContactGrid } from './components/contacts';
-import { DepartmentGrid } from './components/departments';
-import { AddressDataForm, FileInput, GenericDataForm } from './components/forms';
-import { SectionTitle, StartOverAgain } from './components/misc';
+import { StartOverAgain } from './components/misc';
+import {
+  AddressData, AppInfo, ContactData, DepartmentsData, GenericData
+} from './components/parts';
 import store from './store';
 
 const App = (() => {
@@ -12,43 +12,19 @@ const App = (() => {
 
   useLang('pl');
   useTitle(appTitle);
-  useMeta({name: 'author', content: 'Jarek Zgoda'});
+  useMeta({ name: 'author', content: 'Jarek Zgoda' });
+  useMeta({ name: 'descritpion', content: 'Program do edycji danych instancji BIP' });
 
   const clearData = (() => store.reset());
 
   return (
     <Provider store={store}>
       <div class='container grid-lg my-2'>
-        <h1>{appTitle}</h1>
-        <FileInput />
-        <p>
-          Wszystkie wprowadzone poniżej dane są wyświetlane w serwisie BIP jako dane
-          instytucji.
-        </p>
-        <p>
-          We wszystkich poniższych formularzach wypełnienie pól oznaczonych czerwoną
-          gwiazdką (<span class='label-required-marker'>*</span>) jest wymagane.
-        </p>
-        <hr />
-        <SectionTitle title='Dane podstawowe' />
-        <p>Podstawowe dane instytucji.</p>
-        <GenericDataForm />
-        <hr />
-        <SectionTitle title='Dane adresowe' />
-        <p>Dane adresowe instytucji.</p>
-        <AddressDataForm />
-        <hr />
-        <SectionTitle title='Dane kontaktowe' />
-        <p>Dane kontaktowe instytucji.</p>
-        <ContactGrid />
-        <hr />
-        <SectionTitle title='Dane wydziałów i pracowników' />
-        <p>
-          Dane jednostek organizacyjnych instytucji oraz pracowników w nich
-          zatrudnionych.
-        </p>
-        <DepartmentGrid />
-        <hr />
+        <AppInfo appTitle={appTitle} />
+        <GenericData />
+        <AddressData />
+        <ContactData />
+        <DepartmentsData />
         <StartOverAgain clearFunc={clearData} />
       </div>
     </Provider>
---
+++ src/components/parts.js
@@ -0,0 +1,71 @@
+import { ContactGrid } from './contacts';
+import { DepartmentGrid } from './departments';
+import { AddressDataForm, FileInput, GenericDataForm } from './forms';
+import { SectionTitle } from './misc';
+
+const AppInfo = (({ appTitle }) => {
+  return (
+    <>
+      <h1>{appTitle}</h1>
+      <FileInput />
+      <p>
+        Wszystkie wprowadzone poniżej dane są wyświetlane w serwisie BIP jako dane
+        instytucji.
+      </p>
+      <p>
+        We wszystkich poniższych formularzach wypełnienie pól oznaczonych czerwoną
+        gwiazdką (<span class='label-required-marker'>*</span>) jest wymagane.
+      </p>
+      <hr />
+    </>
+  )
+});
+
+const GenericData = (() => {
+  return (
+    <>
+      <SectionTitle title='Dane podstawowe' />
+      <p>Podstawowe dane instytucji.</p>
+      <GenericDataForm />
+      <hr />
+    </>
+  )
+});
+
+const AddressData = (() => {
+  return (
+    <>
+      <SectionTitle title='Dane adresowe' />
+      <p>Dane adresowe instytucji.</p>
+      <AddressDataForm />
+      <hr />
+    </>
+  )
+});
+
+const ContactData = (() => {
+  return (
+    <>
+      <SectionTitle title='Dane kontaktowe' />
+      <p>Dane kontaktowe instytucji.</p>
+      <ContactGrid />
+      <hr />
+    </>
+  )
+});
+
+const DepartmentsData = (() => {
+  return (
+    <>
+      <SectionTitle title='Dane wydziałów i pracowników' />
+      <p>
+        Dane jednostek organizacyjnych instytucji oraz pracowników w nich
+        zatrudnionych.
+      </p>
+      <DepartmentGrid />
+      <hr />
+    </>
+  )
+});
+
+export { AddressData, AppInfo, ContactData, DepartmentsData, GenericData };
