190e06ef093e72ad432ed070fb57966f37bfa7ba
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-03-16 01:34:27 +0100

    What broke?

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,5 @@
 import transformHookNames from 'babel-plugin-transform-hook-names';
+import { DefinePlugin } from 'webpack';
 
 export default (config, env, helpers) => {
   let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
@@ -6,4 +7,20 @@ export default (config, env, helpers) => {
   
   babelConfig.plugins.push(require.resolve('@babel/plugin-transform-react-jsx-source'));
   babelConfig.plugins.push(transformHookNames);
-};
+
+  config.plugins.push(
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    })
+  )
+
+  config.node = {
+    console: false,
+    global: true,
+    process: true,
+    __filename: 'mock',
+    __dirname: 'mock',
+    Buffer: true,
+    setImmediate: true    
+  }
+}
