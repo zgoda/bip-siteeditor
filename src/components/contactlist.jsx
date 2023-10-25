b8ddc17f7ab69ef965bbdbe5d7734b488f2b2340
Jarek Zgoda <jarek.zgoda@gmail.com>
2023-10-25 19:02:42 +0200

    Data labels

--- src/components/contactlist.jsx
+++ src/components/contactlist.jsx
@@ -1,10 +1,21 @@
+import { useContext } from 'preact/hooks';
+import { AppState } from '../state';
+
 export function ContactList() {
+  const state = useContext(AppState);
+
   return (
     <>
       <h2>Dane kontaktowe</h2>
       <details>
         <summary>Kontakt do pracowników instytucji</summary>
+        {state.contactsData.value.map((contact) => {
+          return <p key={contact.name}>{contact.name}</p>;
+        })}
       </details>
+      <button class="secondary outline autowidth">
+        Dodaj dane kontaktowe pracownika
+      </button>
     </>
   );
 }
--- src/components/departmentlist.jsx
+++ src/components/departmentlist.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'preact/hooks';
+import { AppState } from '../state';
+
 export function DepartmentList() {
+  const state = useContext(AppState);
+
   return (
     <>
       <h2>Działy</h2>
       <details>
         <summary>
           Działy organizacyjne instytucji wraz z danymi kontaktowymi pracowników
+          {state.departmentsData.value.map((department) => {
+            return <p key={department.name}>{department.name}</p>;
+          })}
         </summary>
       </details>
+      <button class="secondary outline autowidth">Dodaj dane kontaktowe działu</button>
     </>
   );
 }
